82d4f1464e023a2f1181ca7c3c625ab971c20472
arstgr <78465331+arstgr@users.noreply.github.com>
2022-06-08 22:24:12 +0000

    scripts for running FIO benchmark were added

---
+++ fio_pssh_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export wdir=$(pwd)
+
+sudo yum install pssh -y
+
+echo "beginning date: $(date)"
+echo $(hostname | tr "[:upper:]" "[:lower:]") > hosts.txt
+
+if command -v pbsnodes --version &> /dev/null
+then
+	pbsnodes -avS | grep free | awk -F ' ' '{print tolower($1)}' >> hosts.txt
+fi
+
+pssh -p 301 -t 0 -i -h hosts.txt "cd $wdir && ./fio_test.sh" >> fio_pssh.log 2>&1
+
+
+sleep 10
+
+IFS=$'\n' read -d '' -r -a names < ./hosts.txt
+echo -e "SYSTEM\tWrTPT(MiB/s)\tWrIOPS(k)\tRdTPT(MiB/s)\tRdIOPS(k)" > fio-test-results.log
+echo "*****************************************************************" >> fio-test-results.log
+for i in ${names[@]}; do
+	readarray -t arr <fio-${i}.log
+	echo -e "${i}\t$(echo ${arr[0]} | awk '{print $2}')\t$(echo ${arr[1]} | awk '{print $2}')\t\t$(echo ${arr[2]} | awk '{print $2}')\t\t$(echo ${arr[3]} | awk '{print $2}')" >> fio-test-results.log
+done
+
+
+for i in ${names[@]}; do
+	readarray -t arr <fio-${i}.log
+	echo -e "system: ${i}\tWrTPT: $(echo ${arr[0]} | awk '{print $2}')\tMiB/s" >> fio-wr-tpt-results.log
+	echo -e "system: ${i}\tWrIOPS: $(echo ${arr[1]} | awk '{print $2}')\tk" >> fio-wr-iops-results.log
+	echo -e "system: ${i}\tRdTPT: $(echo ${arr[2]} | awk '{print $2}')\tMiB/s" >> fio-rd-tpt-results.log
+	echo -e "system: ${i}\tRdIOPS: $(echo ${arr[3]} | awk '{print $2}')\tk" >> fio-rd-iops-results.log
+done
+echo "end date: $(date)"
---
+++ fio_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo yum install fio -y
+
+TEMP_DIR=$(mount | grep sdb1 | awk '{print $3}')/fiotest 
+mkdir -p $TEMP_DIR
+
+rm $TEMP_DIR/*
+
+echo FIOWriteTPT: $(fio --name=write_throughput --directory=$TEMP_DIR --numjobs=16 --size=10G --time_based --runtime=120s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=write --group_reporting=1 | grep "WRITE:" | awk '{print $2}' | cut -d '=' -f2 | cut -d 'M' -f1) MiB/s | tee fio-$(hostname  | tr "[:upper:]" "[:lower:]").log 
+
+rm $TEMP_DIR/*
+
+echo FIOWriteIOPS: $(fio --name=write_iops --directory=$TEMP_DIR --size=2G --numjobs=2 --time_based --runtime=300s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=4K --iodepth=128 --rw=randwrite --group_reporting=1 | grep "write: IOPS=" | awk '{print $2}' | cut -d '=' -f2 | cut -d 'k' -f1) K | tee -a fio-$(hostname  | tr "[:upper:]" "[:lower:]").log
+
+rm $TEMP_DIR/*
+
+echo FIOReadTPT: $(fio --name=read_throughput --directory=$TEMP_DIR --numjobs=16 --size=10G --time_based --runtime=120s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=1M --iodepth=64 --rw=read --group_reporting=1  | grep "READ:" | awk '{print $2}' | cut -d '=' -f2 | cut -d 'M' -f1) MiB/s | tee -a fio-$(hostname | tr "[:upper:]" "[:lower:]").log
+
+rm $TEMP_DIR/*
+
+echo FIOReadIOPS: $(fio --name=read_iops --directory=$TEMP_DIR --size=2G --numjobs=2 --time_based --runtime=240s --ramp_time=2s --ioengine=libaio --direct=1 --verify=0 --bs=4K --iodepth=128 --rw=randread --group_reporting=1 | grep "read: IOPS=" | awk '{print $2}' | cut -d '=' -f2 | cut -d 'k' -f1) K | tee -a fio-$(hostname | tr "[:upper:]" "[:lower:]").log
+
+rm $TEMP_DIR/*
+
